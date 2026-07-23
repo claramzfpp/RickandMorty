71d2eaff4e2a78a773225f7b4f64ba383b8f056b
Clara Muniz <140444450+claramzfpp@users.noreply.github.com>
2026-07-23 12:20:24 -0300

    adding project colors

--- app/src/main/java/com/studyProject/rickandmorty/ui/theme/Theme.kt
+++ app/src/main/java/com/studyProject/rickandmorty/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = RMGreen,
+    secondary = RMBrown,
+    tertiary = RMYellow
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = RMGreen,
+    secondary = RMGray,
+    tertiary = RMRed
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
