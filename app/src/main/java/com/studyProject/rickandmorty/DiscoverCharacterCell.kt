91609c7aa24f6947c4643709221d215f30ea784f
Clara Muniz <140444450+claramzfpp@users.noreply.github.com>
2026-07-23 13:01:03 -0300

    created DiscoverCharacterCell

---
+++ app/src/main/java/com/studyProject/rickandmorty/DiscoverCharacterCell.kt
@@ -0,0 +1,94 @@
+package com.studyProject.rickandmorty
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.studyProject.rickandmorty.ui.theme.RMBrown
+import com.studyProject.rickandmorty.ui.theme.RMGray
+import com.studyProject.rickandmorty.ui.theme.RMGreen
+import com.studyProject.rickandmorty.ui.theme.RickAndMortyTheme
+
+@Composable
+fun DiscoverCharacterCell(name: String, status: String, image: String) {
+    Box(
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        imageComponent(image)
+
+        labelComponent(name, status)
+    }
+}
+
+@Composable
+fun labelComponent(name: String, status: String) {
+    val roundedCornerShape = RoundedCornerShape(
+        topStart = 0.dp,
+        topEnd = 0.dp,
+        bottomEnd = 8.dp,
+        bottomStart = 8.dp)
+
+    return Column(
+        modifier = Modifier
+            .width(170.dp)
+            .clip(roundedCornerShape)
+            .background(RMBrown)
+            .padding(horizontal = 7.dp, vertical = 5.dp)
+    ) {
+        Text(
+            text = "$name",
+            color = Color.White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Status: $status",
+            color = Color.White,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Composable
+fun imageComponent(image: String) {
+        Box(
+        modifier = Modifier
+            .size(170.dp, 180.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(RMGray)
+    )
+
+    Box(
+        modifier = Modifier
+            .size(170.dp, 180.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .border(3.dp, RMGreen, RoundedCornerShape(8.dp))
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiscoverCharacterCellPreview() {
+    RickAndMortyTheme {
+        DiscoverCharacterCell(
+            "Rick Sanchez",
+            "Alive",
+            "sjbdahsvdjhavs"
+        )
+    }
+}
